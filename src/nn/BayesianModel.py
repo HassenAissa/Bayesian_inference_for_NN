574e08368a844851f6aed2f087db7565b1d94624
Andy Chen <andy.chen121@imperial.ac.uk>
2024-01-24 03:34:50 +0000

    implement layer distribution intervals at single-double list

--- src/nn/BayesianModel.py
+++ src/nn/BayesianModel.py
@@ -11,11 +11,25 @@ class BayesianModel:
         for layer in model.layers:
             self._layers.append(BayesianModel.Layer([w.shape for w in layer.weights]))
         self._n_layers = len(self._layers)
-        self._layers_dtbn_intervals = [0]
-        self._distributions = [None]
+        self._layers_dtbn_intervals = []
+        self._distributions = []
         self._model = model
 
     def apply_distribution(self, distribution: Distribution, start_layer: int, end_layer: int):
+        # intended implementation: dtbn intervals takes the form like [[0,2],[3,4],[5],[6],[7,12]...]
+        # interval may be single (equal) or double (unequal); distribution list each element correspond to an interval
+        if start_layer > end_layer:
+            raise ValueError('starting_layer must be less than end_layer')
+        elif start_layer < 0 or end_layer >= self._n_layers:
+            raise ValueError('out of bounds')
+        interval = [start_layer]
+        if end_layer > start_layer:
+            interval.append(end_layer)
+        for i in range(len(self._layers_dtbn_intervals)):
+            if start_layer > self._layers_dtbn_intervals[i][0]:
+                self._layers_dtbn_intervals = self._layers_dtbn_intervals[:i+1]+[interval]+self._layers_dtbn_intervals[i+1:]
+                self._distributions = self._distributions[:i+1]+[distribution]+self._distributions[i+1:]
+                break
         """
         TODO: Implement this without errors
         if start_layer > end_layer:
