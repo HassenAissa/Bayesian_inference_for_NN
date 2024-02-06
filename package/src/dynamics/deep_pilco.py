3692aa1b1007ceb55d02114f9906b4551673e060
Andy Chen <xinyangchen4@gmail.com>
2024-02-06 21:32:01 +0000

    dynamic model compatible, fix dimension, set nn without ipd opd

--- package/src/dynamics/deep_pilco.py
+++ package/src/dynamics/deep_pilco.py
@@ -5,13 +5,23 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 import numpy as np
 
+def complete_model(template:tf.keras.models.Sequential, ipd, opd, out_activation):
+    layers = [tf.keras.Input(shape=(ipd,))
+                  ] + template.layers + [
+                      tf.keras.layers.Dense(opd, out_activation)]
+    return tf.keras.models.Sequential(layers)
 
 class NNPolicy(Policy):
     # using tensorflow neural network for optimizing policy params "phi"
-    def __init__(self, network: tf.keras.Model, hyperparams: dict):
-        self.network = network
+    def __init__(self, network, out_activation, hyperparams: dict):
+        self.network = network # template network consisting of inner layers
+        self.out_activation = out_activation
         self.hyperparams = hyperparams
 
+    def setup(self, ipd, opd):
+        self.network = complete_model(self.network, ipd, opd, self.out_activation)
+        
+        
     def optimize_step(self, loss, check_converge=False):
         with tf.GradientTape(persistent=True) as tape:
             grad = tape.gradient(loss, self.network.trainable_variables)
@@ -34,61 +44,68 @@ class NNPolicy(Policy):
 
 class DynamicsTraining:
     # learn dynamic model f for state action transitions
-    def __init__(self, optimizer: Optimizer, hyperparams, base_model, prior, loss, learn_type, normalise):
-        self.optimizer = optimizer
-        self.optimizer.compile(hyperparams, base_model.to_json(),
-                               dataset=None, prior=prior,
-                               starting_model=base_model # TODO: abstract this
-                               )
+    def __init__(self, optimizer:Optimizer, data_specs:list, 
+                 template, out_activation, hyperparams, **kwargs):
+        self.optimizer, self.template, self.out_activation = optimizer, template, out_activation
+        self.hyperparams, self.rems = hyperparams, kwargs
+        self.data_specs = data_specs
+        self.start = False   
 
-        self.loss = loss    # loss function
-        self.learn_type = learn_type    # classification/regression
-        self.normalise = normalise
+    def create_model(self, ipd, opd):
+        model = complete_model(self.template, ipd, opd, self.out_activation) 
+        self.model_config = model.to_json()    
 
-        self.start = False
-        self.ipdim = 1
-        self.tgdim = 1
-
-    def add_transitions(self, all_states, all_actions):
-        if not self.start:
-            for s in all_states.shape:
-                self.ipdim *= s
-            for s in all_actions.shape:
-                self.tgdim *= s
-            self.start = True
-
-        features = []
-        targets = []
-        for s in range(len(all_states)-1):
-            features.append(tf.concat(tf.reshape(all_states[s], [self.ipdim]),
-                                      tf.reshape(all_states[s+1], [self.ipdim])))
-            targets.append(tf.reshape(all_actions[s], self.tgdim))
-
+    def train(self, features, targets, nb_epochs):
         data = tf.data.Dataset.from_tensor_slices((features, targets))
-        dataset = Dataset(data, self.loss, self.learn_type, self.normalise)
-        self.train_dataset = Dataset(
-            dataset.train_data, self.loss, self.learn_type, self.normalise)
-
-    def train(self, nb_epochs):
-        self.optimizer._dataset = self.train_dataset
+        dataset = Dataset(data, *self.data_specs)
+        train_dataset = Dataset(
+            dataset.train_data, *self.data_specs)
+        if not self.start:
+            self.optimizer.compile(self.hyperparams, self.model_config, 
+                                   train_dataset, **self.rems)
+            self.start = True
+        else:
+            self.optimizer._dataset = train_dataset
         self.optimizer.train(nb_epochs)
 
 
 class BayesianDynamics(Control):
     def __init__(
-        self, env: gym.Env, n_episodes, policy, state_reward, dyntrain_config: list, learn_config: tuple,
-        # policy_optimizer: PolicyOptimizer
+        self, env: gym.Env, horizon:int, dyn_training:DynamicsTraining,
+        policy, state_reward, learn_config:tuple
     ):
-        super().__init__(env, n_episodes, policy, state_reward)
+        self.state_d = env.observation_space.shape
+        self.action_d = env.action_space.shape
+        self.action_fd = 1
+        for d in env.action_space.shape:
+            self.action_fd *= d
+        self.state_fd = 1
+        for d in self.state_d:
+            self.state_fd *= d
+        dyn_training.create_model(
+            self.state_fd+self.action_fd, self.state_fd)
+        policy.setup(self.state_d, self.action_fd)
+        super().__init__(env, horizon, policy)
         # Bayesian Model optimizer learning state-action-transition "f"
-        self.training = DynamicsTraining(*dyntrain_config)
-        self.dyntrain_epochs, self.kp, self.gamma = learn_config
+        self.dyn_training = dyn_training
+        self.state_reward = state_reward
+        self.dyntrain_ep, self.kp, self.gamma = learn_config
         # self.policy_optimizer = policy_optimizer
 
     def sample_initial(self):
         # default sampling method
-        print("init sample from bayesian dynamics")
+        print("initial sample from bayesian dynamics")
         return self.env.observation_space.sample()
+    
+    def execute(self):
+        all_states, all_actions = super().execute()
+        features = []
+        targets = []
+        for s in range(len(all_states)-1):
+            features.append(tf.concat(tf.reshape(all_states[s], [self.state_fd]),
+                            tf.reshape(all_actions[s], [self.action_fd])))
+            targets.append(tf.reshape(all_states[s+1], [self.state_fd]))
+        return features, targets
 
     def k_particles(self, k):
         # create k random bnn weights and k random inputs
@@ -105,7 +122,7 @@ class BayesianDynamics(Control):
         self.k_particles(k)
         xs = self.inputs
         traj = [xs]
-        for t in self.n_episodes:
+        for t in self.horizon:
             ys = []
             for i in range(k):
                 ys.append(self.models[i](xs[i]))
@@ -135,9 +152,8 @@ class BayesianDynamics(Control):
     def learn(self, nb_epochs):
         def step(check_converge=False):
             # train dynamic model using transition dataset
-            all_states, all_actions = self.execute()
-            self.training.add_transitions(all_states, all_actions)
-            self.training.train(self.dyntrain_epochs)
+            xs, ys = self.execute()
+            self.dyn_training.train(xs, ys, self.dyntrain_ep)
             # predict trajectory
             traj = self.predict_trajectory(self.kp)
             # evaluate policy and trajectory
