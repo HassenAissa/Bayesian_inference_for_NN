d7fca8cca275e7ce62ef45ae4ce0540ff2cd2eea
Tony AdAstra <67339555+tonyadastra@users.noreply.github.com>
2024-02-06 01:37:51 +0000

    policy optimizer (hope im doing something useful here?)

--- package/src/dynamics/control.py
+++ package/src/dynamics/control.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 import gymnasium as gym
 from abc import ABC, abstractmethod
 
@@ -14,6 +15,19 @@ class Policy(ABC):  # Policy optimizer
 
     @abstractmethod
     def act(self, state):
+        pass
+    
+class PolicyOptimizer(ABC):
+    @abstractmethod
+    def compile(self, model, hyperparams):
+        pass
+
+    @abstractmethod
+    def train(self, dataset, epochs):
+        pass
+
+    @abstractmethod
+    def optimize(self, loss, check_convergence=True):
         pass
 
 class Control(ABC):
@@ -52,6 +66,53 @@ class Control(ABC):
     @abstractmethod
     def learn(self):
         pass
+    
+
+class NNPolicyOptimizer(PolicyOptimizer):
+    """Neural network policy optimizer for reinforcement learning
+        model: tf.keras.Model - neural network model
+        hyperparams: dict - hyperparameters for the optimizer
+        convergence_threshold: float - threshold for convergence
+        previous_loss: float - previous loss value, keeps track of previous loss for convergence check
+    """
+
+    def __init__(self, model: tf.keras.Model, hyperparams: dict):
+        self.model = model
+        self.hyperparams = hyperparams
+        self.optimizer = tf.keras.optimizers.get(hyperparams.get("optimizer", "adam"))
+        
+        self.convergence_threshold = hyperparams.get("convergence_threshold", 1e-4)
+        self.previous_loss = None
+
+    def compile(self, model, hyperparams):
+        # Compile the neural network model with the given hyperparameters
+        self.model = model
+        self.model.compile(optimizer=self.optimizer, loss=hyperparams["loss"])
+
+    def train(self, dataset, epochs):
+        # Train the model on the given dataset for a specified number of epochs
+        self.model.fit(dataset, epochs=epochs)
+        
+    def check_convergence(self, loss):
+        # Check for convergence
+        if self.previous_loss is not None:
+            # Check if absolute difference between previous and current loss is less than the threshold
+            if abs(loss - self.previous_loss) < self.convergence_threshold:
+                return True
+        self.previous_loss = loss
+        return False
+
+    def optimize(self, loss, check_convergence=True):
+        # Perform an optimization step to update the policy parameters
+        with tf.GradientTape() as tape:
+            # Calculate gradients
+            gradients = tape.gradient(loss, self.model.trainable_variables)
+        # Apply gradients to the model
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+        
+        # Check for convergence
+        if check_convergence:
+            return self.check_convergence(loss)
 
 
 
--- package/src/dynamics/deep_pilco.py
+++ package/src/dynamics/deep_pilco.py
@@ -1,13 +1,14 @@
-from src.dynamics.control import gym, Policy, Control
+from src.dynamics.control import gym, Policy, Control, PolicyOptimizer
 from src.datasets.Dataset import Dataset
 from src.optimizers.Optimizer import Optimizer
 import tensorflow as tf
 import tensorflow_probability as tfp
 import numpy as np
 
+
 class NNPolicy(Policy):
     # using tensorflow neural network for optimizing policy params "phi"
-    def __init__(self, network: tf.keras.Model, hyperparams:dict):
+    def __init__(self, network: tf.keras.Model, hyperparams: dict):
         self.network = network
         self.hyperparams = hyperparams
 
@@ -27,20 +28,21 @@ class NNPolicy(Policy):
             converge = False
             return converge
 
-
     def act(self, state):
         return self.network(state)
 
+
 class DynamicsTraining:
     # learn dynamic model f for state action transitions
-    def __init__(self, optimizer:Optimizer, hyperparams, base_model, prior, loss, learn_type, normalise):
+    def __init__(self, optimizer: Optimizer, hyperparams, base_model, prior, loss, learn_type, normalise):
         self.optimizer = optimizer
-        self.optimizer.compile(hyperparams, base_model, dataset=None, prior=prior)
-        
+        self.optimizer.compile(hyperparams, base_model,
+                               dataset=None, prior=prior)
+
         self.loss = loss    # loss function
         self.learn_type = learn_type    # classification/regression
         self.normalise = normalise
-        
+
         self.start = False
         self.ipdim = 1
         self.tgdim = 1
@@ -56,29 +58,36 @@ class DynamicsTraining:
         features = []
         targets = []
         for s in range(len(all_states)-1):
-            features.append(tf.concat(tf.reshape(all_states[s], [self.ipdim]), 
-                                       tf.reshape(all_states[s+1], [self.ipdim])))
+            features.append(tf.concat(tf.reshape(all_states[s], [self.ipdim]),
+                                      tf.reshape(all_states[s+1], [self.ipdim])))
             targets.append(tf.reshape(all_actions[s], self.tgdim))
-        
+
         data = tf.data.Dataset.from_tensor_slices((features, targets))
         dataset = Dataset(data, self.loss, self.learn_type, self.normalise)
-        self.train_dataset = Dataset(dataset.train_data, self.loss, self.learn_type, self.normalise)
+        self.train_dataset = Dataset(
+            dataset.train_data, self.loss, self.learn_type, self.normalise)
 
     def train(self, nb_epochs):
         self.optimizer._dataset = self.train_dataset
         self.optimizer.train(nb_epochs)
 
+
 class BayesianDynamics(Control):
-    def __init__(self, env:gym.Env, n_episodes, policy, state_reward, dyntrain_config:list, learn_config:tuple):
+    def __init__(
+        self, env: gym.Env, n_episodes, policy, state_reward, dyntrain_config: list, learn_config: tuple,
+        policy_optimizer: PolicyOptimizer
+    ):
         super.__init__(env, n_episodes, policy, state_reward)
-        self.training = DynamicsTraining(*dyntrain_config)  # Bayesian Model optimizer learning state-action-transition "f"
+        # Bayesian Model optimizer learning state-action-transition "f"
+        self.training = DynamicsTraining(*dyntrain_config)
         self.dyntrain_epochs, self.kp, self.gamma = learn_config
-    
+        self.policy_optimizer = policy_optimizer
+
     def sample_initial(self):
         # default sampling method
         print("init sample from bayesian dynamics")
         return self.env.observation_space.sample()
-    
+
     def k_particles(self, k):
         # create k random bnn weights and k random inputs
         self.models = []
@@ -88,7 +97,7 @@ class BayesianDynamics(Control):
             bnn._sample_weights()
             self.models.append(bnn._model.copy())
             self.inputs.append(self.sample_initial())
-    
+
     def predict_trajectory(self, k):
         # Step 6 of pilco algorithm using k particles
         self.k_particles(k)
@@ -101,7 +110,7 @@ class BayesianDynamics(Control):
             ymean = np.mean(ys)
             ystd = np.std(ys)
             dtbn = tfp.distributions.Normal(ymean, ystd)
-            xs = []            
+            xs = []
             for i in range(k):
                 x = dtbn.sample()
                 xs.append(x)
@@ -115,12 +124,12 @@ class BayesianDynamics(Control):
             k_rew = 0
             for s in states:
                 # if calculating cost, use negative state reward
-                k_rew += self.state_reward(s)  
+                k_rew += self.state_reward(s)
             k_rew /= self.kp
             tot_rew += discount * k_rew
             discount *= self.gamma
         return tot_rew
-    
+
     def learn(self, nb_epochs):
         def step(check_converge=False):
             # train dynamic model using transition dataset
@@ -131,9 +140,14 @@ class BayesianDynamics(Control):
             traj = self.predict_trajectory(self.kp)
             # evaluate policy and trajectory
             tot_rew = self.reward(traj)
-            # optimize policy
-            return self.policy.optimize_step(loss=-tot_rew, check_converge=check_converge)
+            # # optimize policy
+            # return self.policy.optimize_step(loss=-tot_rew, check_converge=check_converge)
             
+            # optimize policy using the policy optimizer
+            loss = -tot_rew  # Define the loss as the negative of the total reward
+            policy_loss = self.policy_optimizer.optimize(self.policy, loss, check_converge)
+            return policy_loss
+
         if nb_epochs:
             # learning for a given number of epochs
             for ep in range(nb_epochs):
@@ -142,12 +156,3 @@ class BayesianDynamics(Control):
             while not step(check_converge=True):
                 # continue learning if policy not converge
                 continue
-        
-                
-
-
-
-
-            
-            
-
