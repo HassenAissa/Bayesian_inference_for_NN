9adc15079e70d33d0dcf757a0e02f9e8d973197a
Hassen Aissa <hassen.aissa@epfl.ch>
2024-02-20 12:24:16 +0000

    test on mnist

--- PyAce/tests/tf_dataset_test.py
+++ PyAce/tests/tf_dataset_test.py
@@ -7,7 +7,7 @@ from PyAce.datasets import Dataset
 from PyAce.nn import BayesianModel
 from PyAce.optimizers import HyperParameters
 from PyAce.optimizers import SWAG
-from PyAce.visualisations import Visualisation
+from PyAce.visualisations import Metrics
 from PyAce.visualisations import Robustness
 import tensorflow_datasets as tfds
 
@@ -32,31 +32,20 @@ def runner():
     base_model.add(tf.keras.layers.Dense(84, activation='relu'))
     base_model.add(tf.keras.layers.Dense(62, activation=tf.keras.activations.softmax))
 
-    hyperparams = HyperParameters(lr=1e-3, alpha = 0.00)
+    hyperparams = HyperParameters(lr=1e-1, alpha = 1/4096, pi =1, batch_size = 4096)
     # instantiate your optimizer
     optimizer = BBB()
-    prior = GaussianPrior(.0,-10.0)
-
-    # compile the optimizer with your data
-    # this is a specification of SWAG, SWAG needs a starting_model from which to start the gradient descend
+    prior = GaussianPrior(.0,-5.0)
     optimizer.compile(hyperparams, base_model.to_json(), dataset, prior = prior)
-
-    loss_save_file = r"package/src/visualisations/loss_save_file"
-    optimizer.train(100, loss_save_file)
+    optimizer.train(1000)
 
 
 
     bayesian_model: BayesianModel = optimizer.result()
-    # store_path = r"..."
-    # bayesian_model.store(store_path)
-    # bayesian_model: BayesianModel= BayesianModel.load(store_path)
-
-    analytics_builder = Visualisation(bayesian_model)
-    robustness_builder = Robustness(bayesian_model)
-
+    analytics_builder = Metrics(bayesian_model, dataset)
     print("Starting performence analysis")
-    analytics_builder.visualise(dataset, 100, loss_save_file)
-    #print("Starting robustness analysis")
-    #robustness_builder.c_robustness(dataset, 100)
+    analytics_builder.summary(100)
 
 
+
+runner()
