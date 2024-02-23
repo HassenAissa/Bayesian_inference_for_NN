6131f29c67517dcd5ad685eab27b3bcc6d082c62
rosa-quere <rosaquere9@gmail.com>
2024-02-23 10:20:30 +0000

    Corruption Robustness

--- PyAce/tests/robustness_test.py
+++ PyAce/tests/robustness_test.py
@@ -4,8 +4,8 @@ from tensorflow.keras import models, layers
 from PyAce.datasets import Dataset
 from PyAce.nn import BayesianModel
 from PyAce.optimizers import HyperParameters
-from PyAce.optimizers import SWAG
-from PyAce.visualisations import Visualisation
+from PyAce.optimizers import BBB
+from PyAce.distributions import GaussianPrior
 from PyAce.visualisations import Robustness
 import tensorflow_datasets as tfds
 
@@ -29,26 +29,25 @@ def runner():
     base_model.add(tf.keras.layers.Dense(84, activation='relu'))
     base_model.add(tf.keras.layers.Dense(62, activation=tf.keras.activations.softmax))
 
-    hyperparams = HyperParameters(lr=1e-3, k=50, frequency=1, scale=1)
+    hyperparams = HyperParameters(lr=1e-1, alpha = 1/4096, pi =1, batch_size = 4096)
     # instantiate your optimizer
-    optimizer = SWAG()
+    optimizer = BBB()
+    prior = GaussianPrior(.0,-5.0)
+    optimizer.compile(hyperparams, base_model.to_json(), dataset, prior = prior)
+    optimizer.train(1000)
 
-    # compile the optimizer with your data
-    # this is a specification of SWAG, SWAG needs a starting_model from which to start the gradient descend
-    optimizer.compile(hyperparams, base_model.to_json(), dataset, starting_model=base_model)
 
-    loss_save_file = r"package/src/visualisations/loss_save_file"
-    optimizer.train(1000, loss_save_file)
 
     bayesian_model: BayesianModel = optimizer.result()
-    # store_path = r"..."
-    # bayesian_model.store(store_path)
-    # bayesian_model: BayesianModel= BayesianModel.load(store_path)
-
-    analytics_builder = Visualisation(bayesian_model)
-    robustness_builder = Robustness(bayesian_model)
-
-    #print("Starting performence analysis")
-    #analytics_builder.visualise(dataset, 100, loss_save_file)
     print("Starting robustness analysis")
-    robustness_builder.c_robustness(dataset, 10)
+    path = "PyAce/tests"
+    robustness_builder = Robustness.Robustness(bayesian_model, dataset)
+    corruptions = ["gaussian_noise", "shot_noise", "impulse_noise", "speckle_noise",
+                            "gaussian_blur", "contrast", "brightness", "saturate","pixelate"]
+    for c in corruptions:
+        robustness_builder.corruption_error(c)
+    
+    robustness_builder.mean_corruption_error()
+    robustness_builder.mean_corruption_error(relative=True, save_path=path)
+        
+runner()
