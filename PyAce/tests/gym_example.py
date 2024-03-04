a2fa6ce534bd8e44ef99c2ce2a1bbd54686da352
Hassen Aissa <hassen.aissa@epfl.ch>
2024-03-04 14:58:27 +0000

    deepPilco

--- PyAce/tests/gym_example.py
+++ PyAce/tests/gym_example.py
@@ -6,97 +6,68 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 from PyAce.datasets import Dataset
+from PyAce.optimizers.hyperparameters import HyperParameters
+from PyAce.dynamics import DeepPilco
+from tensorflow.keras import models, layers
+from PyAce.optimizers import SWAG
+import time
 
+def h2(state):
+    return state[0]*state[2] - state[1]*state[3]
+
+def ht_speed(state):
+    height = 4-state[0] - state[0]*2 - h2(state)
+    speed = pow(state[4], 2)
+    return -(height)
 
 def runner():
-    # Set up the environment
-    env = gym.make("LunarLander-v2", render_mode="human", )
-    x_prev, info = env.reset(seed=42)
+    policy = tf.keras.models.Sequential()
+    policy.add(layers.Dense(10, activation='relu', input_shape=(6,)))
+    policy.add(layers.Dense(3, activation=tf.keras.activations.softmax))
+    policy_hyperparams = HyperParameters(lr = 1e-3)
 
+    bayesian_model = tf.keras.models.Sequential()
+    bayesian_model.add(layers.Dense(30, activation='relu', input_shape=(9,)))
+    bayesian_model.add(layers.Dense(6, activation="linear"))
+    bayesion_hyperparams = HyperParameters(batch_size= 10, lr = 1e-1, k = 30, scale = 1, frequency = 1)
 
-    # bayesian dynamics training
-    """
-    TODO:  set up the bayesian model and policy for gym env
+    env = gym.make("Acrobot")
+    deep_pilco = DeepPilco(
+        env= env,
+        policy= policy,
+        policy_optimizer= tf.keras.optimizers.Adam(learning_rate=1e-3),
+        bayesian_model_json= bayesian_model.to_json(),
+        bayesian_optimizer= SWAG,
+        bayesian_hyperparameters= bayesion_hyperparams,
+        n_optimizer_iterations= 1000,
+        extra_parameters_for_optimizer= {"starting_model": bayesian_model},
+        reward=ht_speed,
+        horizon=10
+    )
 
-    bnn = prior
-    policy = prior
+    deep_pilco.learn(10)
 
-    D = create_dataset(env)
-    episodes = 1000
-    bnn.train(D, epochs=episodes)
-    policy.optimize(D, bnn, epochs=episodes)
-
-    reward = 0
-    for i in range(episodes):
-        x_t = env.reset()
-        H = horizon
-        for t in range(H):
-            a_t = policy.act(x_t)
-            x_t1, r_t, done, _ = env.step(a_t)
-            D.append(x_t, a_t, r_t, x_t1)
-            x_t = x_t1
-            if done:
-                break
-    """
-
-
-    # plt.ion()  # Turn on interactive mode for non-blocking plotting
-    fig, ax = plt.subplots()
-    xdata, ydata = [], []
-
-    # Initialize the plot
-    ax.set_xlim(0, 1000)
-    ax.set_ylim(-500, 300)  # Adjust the y-axis limits according to expected rewards
-    line, = ax.plot(xdata, ydata, 'r-', label='Reward per Episode')
-    ax.set_title('Reward Graph')  # Title for the graph
-    ax.set_xlabel('Episode')  # X-axis label
-    ax.set_ylabel('Total Reward')  # Y-axis label
-    ax.legend()  # Add a legend
-
-    def update_plot(x, y):
-        line.set_xdata(x)
-        line.set_ydata(y)
-        ax.relim()
-        ax.autoscale_view()
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-
-
-    x_array = []
-    y_array = []
-
-
-    # Run the game loop
-    total_reward = 0
-    for i in range(1000):
-        action = env.action_space.sample()  # this is where you would insert your policy
-        # policy.act(x_prev)
-        
-        x, reward, terminated, truncated, info = env.step(action)
-        x_data = np.append(x_prev, action)
-        
-        x_array.append(x_data)
-        y_array.append(x)
-        
-        # total_reward += reward
-        
+    env = gym.make("Acrobot", render_mode="human")  # Use "human" render mode for visualization
+    observation, info = env.reset(seed=42)
+    done = False
+    rewards, total_reward = [], 0
+    states = []
+    actions = []
+    while not done:
+        states.append(observation)
+        # normalize observation -1 to 1 when taking policy; 
+        # action take is the actual action taken without normalization
+        action_taken = policy.predict(tf.reshape(tf.convert_to_tensor(observation), (1,-1)))
+        action_taken = tf.argmax(action_taken, axis = 1)
+        actions.append(action_taken[0])
+        observation, reward, terminated, truncated, info = env.step(action_taken[0].numpy())
+        # reward = ht_speed(observation)
+        total_reward += reward  # Accumulate the reward
+        rewards.append(total_reward)
         if terminated or truncated:
-        #     xdata.append(i)
-        #     ydata.append(total_reward)
-        #     update_plot(xdata, ydata)  # Update the plot with the latest reward
-        #     total_reward = 0
-            x_prev, info = env.reset()  # Reset the environment
-        
-        x_prev = x  # Update the previous state
-        
-        # print(x_data)
-        # print(x)
-        # print(x_array)
-        # print(y_array)
-
-    env.close()  # Close the environment when done
-
-    # convert to tf.data.Dataset
-    dataset: tf.data.Dataset = tf.data.Dataset.from_tensor_slices((x_array, y_array))
-
-    print(dataset)
+            done = True
+        # You can add a delay here if the visualization is too fast
+        time.sleep(0.02)
+    print(rewards)
+    print(actions)
+runner()
